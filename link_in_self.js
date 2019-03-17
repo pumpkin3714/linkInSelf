5d2ef343f9bbbccce169b6f28ca58e01819385a8
pumpkin3714 <pupmpkin3714@gmail.com>
2019-03-18 03:51:41 +0900

    fix some bugs and typos

--- link_in_self.js
+++ link_in_self.js
@@ -1,21 +1,20 @@
-(function(){
-    var getdomain = function(url){
-        if(/^.{1,10}:\/\//.test(url)){
+(function () {
+    var getdomain = function (url) {
+        if (/^.{1,10}:\/\//.test(url)) {
             url = url.split('//')[1];
+            return url.split('/')[0];
         }
-        return url.split('/')[0];
+        return getdomain(location.href);
     }
-    
-    var anchors = document.getElementsByTag('a');
-    for(var i = 0; i < anchors.length; i++){
+
+    var anchors = document.getElementsByTagName('a');
+    for (var i = 0; i < anchors.length; i++) {
         var tgt = anchors[i].getAttribute('target');
         var pagedomain = getdomain(location.href);
-        var anchordomain = getdomain(anchors[i].getAttribute('href'));
-        
-        if(pagedomain == anchordomain && tgt && tgt != '_self') {
+        var anchordomain = getdomain();
+
+        if (anchordomain && pagedomain == anchordomain && tgt && tgt != '_self') {
             anchors[i].setAttribute('target', '_self');
         }
-        
-        console.log(anchors[i]);
     }
 })();
